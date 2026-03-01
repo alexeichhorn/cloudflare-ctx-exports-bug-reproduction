a5d62f136634c4527332473e4b3304ecd7b83175
Alexander Eichhorn <a.eichhorn@losjet.com>
2026-03-01 17:51:48 +0100

    removed worker loader since this might be not core to the problem

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ export class OutboundProbe extends WorkerEntrypoint {
 }
 
 export default {
-  async fetch(request, env, ctx): Promise<Response> {
+  async fetch(request, _env, ctx): Promise<Response> {
     const url = new URL(request.url);
     console.log('[index.fetch] URL:', request.url);
 
@@ -20,35 +20,10 @@ export default {
       const directRpc = await ctx.exports.OutboundProbe({}).someOtherFunc();
       const directFetchText = await (await ctx.exports.OutboundProbe({}).fetch(new Request('https://example.com/repro'))).text();
 
-      const worker = env.LOADER.get('repro-worker', () => ({
-        compatibilityDate: '2026-02-25',
-        mainModule: 'main.js',
-        modules: {
-          'main.js': {
-            js: `
-              export default {
-                async fetch() {
-                  const r = await fetch('https://example.com/from-loader');
-                  const text = await r.text();
-                  return new Response(JSON.stringify({ text }), {
-                    headers: { 'content-type': 'application/json' },
-                  });
-                }
-              };
-            `,
-          },
-        },
-        globalOutbound: ctx.exports.OutboundProbe({}),
-      }));
-
-      const loaderResponse = await worker.getEntrypoint().fetch('https://loader-entry/probe');
-      const loaderJson = (await loaderResponse.json()) as { text: string };
-
       return Response.json({
         directRpc,
         directFetchText,
-        loaderFetchText: loaderJson.text,
-        note: 'If this were hitting OutboundProbe.fetch, both fetch texts should start with FROM_OUTBOUND_CLASS.',
+        note: 'Direct ctx.exports fetch should hit OutboundProbe.fetch and start with FROM_OUTBOUND_CLASS.',
       });
     }
 
