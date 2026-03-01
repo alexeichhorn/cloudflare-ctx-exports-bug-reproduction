ec5255c2d243f1412a323da3caf6566dd17b11bb
Alexander Eichhorn <a.eichhorn@losjet.com>
2026-03-01 14:11:42 +0100

    simpler worker entrypoint

--- src/services/executor/OutboundProbe.ts
+++ src/services/executor/OutboundProbe.ts
@@ -1,6 +1,6 @@
 import { WorkerEntrypoint } from 'cloudflare:workers';
 
-export class OutboundProbe extends WorkerEntrypoint<Cloudflare.Env, {}> {
+export class OutboundProbe extends WorkerEntrypoint {
   async fetch(request: Request): Promise<Response> {
     console.log('[OutboundProbe.fetch] URL:', request.url, 'mode:', 'no-props');
     return new Response(`FROM_OUTBOUND_CLASS mode=no-props url=${request.url}`);
